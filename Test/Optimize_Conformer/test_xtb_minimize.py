deefa5c1be27c512cb81e6778a99081d769727af
Thanh An Pham <ptayds2001@gmail.com>
2024-10-12 19:23:53 +0100

    add optimize conformer

--- Test/Optimize_Conformer/test_xtb_minimize.py
+++ Test/Optimize_Conformer/test_xtb_minimize.py
@@ -1,31 +1,43 @@
 import unittest
+import tempfile
+import shutil
+import os
 from unittest.mock import patch, MagicMock
 from ProDock.Optimize_Conformer.xtb_minimize import XTBMinimize
+from ProDock.Optimize_Conformer.ligand_conformer import Ligand_Conformer
 
 
 class TestXTBMinimize(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory for testing
+        self.test_dir = tempfile.mkdtemp()
+        self.sdf_path = os.path.join(self.test_dir, "sdf_folder")
+        Ligand_Conformer().write_sdf(
+            smi_filename="Data/smi_testcase.txt", sdf_output_folder=self.sdf_path
+        )
+
     def test_retrive_filename(self):
-        folder = "/Users/anpham/Documents/GitHub/ProDock/delete"
+
         with patch(
             "glob.glob",
             return_value=[
-                "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0",
-                "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_1",
+                os.path.join(self.sdf_path, "ligand_0"),
+                os.path.join(self.sdf_path, "ligand_1"),
             ],
         ):
-            folder_paths, file_names = XTBMinimize.retrive_filename(folder)
+            folder_paths, file_names = XTBMinimize.retrive_filename(self.sdf_path)
             self.assertEqual(
                 folder_paths,
                 [
-                    "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0",
-                    "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_1",
+                    os.path.join(self.sdf_path, "ligand_0"),
+                    os.path.join(self.sdf_path, "ligand_1"),
                 ],
             )
             self.assertEqual(
                 file_names,
                 [
-                    "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0/ligand_0.sdf",
-                    "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_1/ligand_1.sdf",
+                    os.path.join(self.sdf_path, "ligand_0", "ligand_0.sdf"),
+                    os.path.join(self.sdf_path, "ligand_1", "ligand_1.sdf"),
                 ],
             )
 
@@ -42,17 +54,21 @@ class TestXTBMinimize(unittest.TestCase):
         with patch(
             "ProDock.Optimize_Conformer.xtb_minimize.XTBMinimize.retrive_filename",
             return_value=(
-                ["/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0"],
-                ["/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0.sdf"],
+                [os.path.join(self.sdf_path, "ligand_0")],
+                [os.path.join(self.sdf_path, "ligand_0", "ligand_0.sdf")],
             ),
         ), patch(
             "ProDock.Optimize_Conformer.xtb_minimize.XTBMinimize.xtb_optimize"
         ) as mock_optimize:
             minimize = XTBMinimize()
-            minimize.fit("/Users/anpham/Documents/GitHub/ProDock/delete")
+            minimize.fit({self.sdf_path})
             mock_optimize.assert_called_once_with(
-                "/Users/anpham/Documents/GitHub/ProDock/delete/ligand_0.sdf"
+                os.path.join(self.sdf_path, "ligand_0", "ligand_0.sdf")
             )
+
+    def tearDown(self):
+        # Cleanup temporary directory after test
+        shutil.rmtree(self.sdf_path)
 
 
 if __name__ == "__main__":
