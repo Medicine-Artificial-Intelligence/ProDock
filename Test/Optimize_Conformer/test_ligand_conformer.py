97501661f64e98af1676e91683f67845f36ecc95
Thanh An Pham <ptayds2001@gmail.com>
2024-10-12 17:36:41 +0100

    add optimize conformer

--- Test/Optimize_Conformer/test_ligand_conformer.py
+++ Test/Optimize_Conformer/test_ligand_conformer.py
@@ -1,4 +1,6 @@
 import unittest
+import tempfile
+import shutil
 from unittest.mock import patch, MagicMock
 from ProDock.Optimize_Conformer.ligand_conformer import Ligand_Conformer
 
@@ -20,13 +22,14 @@ class TestLigandConformer(unittest.TestCase):
         self, mock_writer, mock_move, mock_makedirs, mock_embedding, mock_list_smi
     ):
         ligand_conformer = Ligand_Conformer()
-        ligand_conformer.write_sdf(
-            "Data/smi_testcase.txt", "/Users/anpham/Documents/GitHub/ProDock/delete"
-        )
+        """Set up a temporary directory and create a test CSV file."""
+        test_dir = tempfile.mkdtemp()
+        ligand_conformer.write_sdf("Data/smi_testcase.txt", test_dir)
         mock_list_smi.assert_called_once_with("Data/smi_testcase.txt")
         mock_embedding.assert_called()
         mock_writer.assert_called()
         mock_move.assert_called()
+        shutil.rmtree(test_dir)
 
 
 if __name__ == "__main__":
